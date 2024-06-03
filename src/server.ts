3501bbaeb8d704d44aee4dc9d16037066db1fa8f
Loc Q. Huynh <quanglochuynh@gmail.com>
2024-06-03 13:54:32 +0700

    add cors

--- src/server.ts
+++ src/server.ts
@@ -2,17 +2,27 @@
 import { dotenvConfig } from "./configs/dotenv.config";
 dotenvConfig();
 import express from "express";
-import {
-  communeCollection,
-  connectMongoDB,
-  districtCollection,
-  provinceCollection,
-} from "./configs/mongo.config";
+import { connectMongoDB } from "./configs/mongo.config";
 import provinceRouter from "./router";
+import cors from "cors";
 
 connectMongoDB();
 
 const app = express();
+
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:5174",
+      "http://localhost:5175",
+      "https://konglass.com",
+      "https://kalacandela.com",
+      "https://congngheoct.com",
+      "https://ongchutu.com",
+    ],
+  })
+);
 
 app.use("/province-api", provinceRouter);
 
